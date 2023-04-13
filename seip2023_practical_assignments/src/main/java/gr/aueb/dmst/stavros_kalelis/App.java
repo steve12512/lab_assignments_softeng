b7656861d248fc1a9548418f47dab17031e13f01
steve12512 <stevekalelis@hotmail.com>
2023-04-13 18:00:00 +0300

    first pom

---
+++ seip2023_practical_assignments/src/main/java/gr/aueb/dmst/stavros_kalelis/App.java
@@ -0,0 +1,13 @@
+package gr.aueb.dmst.stavros_kalelis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
